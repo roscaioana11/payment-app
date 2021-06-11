6ef831da7fcf90f4d048dcd1bb3a22e068e49669
Ioana Rosca <rosca.ioana11@gmail.com>
2021-06-12 00:16:10 +0300

    Setup Payment App

---
+++ payment-service/src/test/java/ro/fasttrackit/homework13/payment/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.fasttrackit.homework13.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
